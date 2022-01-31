d66e741c84777d37e96ebc52714675b3f3d7608d
rwxd <rwxd@pm.me>
2022-01-31 21:44:38 +0100

    fix(*): update

---
+++ main.tf
@@ -0,0 +1,138 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "metallb" {
+  name             = "metallb"
+  repository       = "https://metallb.github.io/metallb"
+  chart            = "metallb"
+  namespace        = "metallb"
+  create_namespace = true
+
+  set {
+    name  = "configInline.address-pools[0].name"
+    value = "default"
+    type  = "string"
+  }
+
+  set {
+    name  = "configInline.address-pools[0].protocol"
+    value = "layer2"
+    type  = "string"
+  }
+
+  set {
+    name  = "configInline.address-pools[0].addresses[0]"
+    value = "192.168.3.200-192.168.3.250"
+    type  = "string"
+  }
+}
+
+
+#resource "helm_release" "project-contour" {
+#  name             = "project-contour"
+#  repository       = "https://charts.bitnami.com/bitnami"
+#  chart            = "contour"
+#  namespace        = "project-contour"
+#  create_namespace = true
+#
+#  depends_on = [helm_release.metallb]
+#
+#  set {
+#    name  = "contour.image.registry"
+#    value = "docker.io"
+#    type  = "string"
+#  }
+#
+#  set {
+#    name  = "contour.image.repository"
+#    value = "projectcontour/contour"
+#    type  = "string"
+#  }
+#
+#  set {
+#    name  = "contour.image.tag"
+#    value = "v1.19.1"
+#    type  = "string"
+#  }
+#
+#  set {
+#    name  = "envoy.image.registry"
+#    value = "docker.io"
+#    type  = "string"
+#  }
+#
+#  set {
+#    name  = "envoy.image.repository"
+#    value = "envoyproxy/envoy"
+#    type  = "string"
+#  }
+#
+#  set {
+#    name  = "envoy.image.tag"
+#    value = "v1.21.0"
+#    type  = "string"
+#  }
+#
+#  set {
+#    name = "envoy.hostNetwork"
+#    value = true
+#  }
+#
+#}
+
+resource "helm_release" "nginx-ingress-controller" {
+  name             = "nginx-ingress-controller"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "nginx-ingress-controller"
+  create_namespace = true
+
+  depends_on = [helm_release.metallb]
+
+  set {
+    name = "defaultBackend.enabled"
+    value = false
+  }
+}
+
+resource "helm_release" "cert-manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = true
+  }
+}
+
+resource "helm_release" "external-dns" {
+  name             = "external-dns"
+  repository       = "https://charts.bitnami.com/bitnami"
+  chart            = "external-dns"
+  namespace        = "external-dns"
+  create_namespace = true
+
+  set {
+    name  = "image.registry"
+    value = "gcr.io"
+    type  = "string"
+  }
+
+  set {
+    name  = "image.repository"
+    value = "k8s-staging-external-dns/external-dns"
+    type  = "string"
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v20220128-external-dns-helm-chart-1.7.1-50-g600111f8-arm64v8"
+    type  = "string"
+  }
+}
